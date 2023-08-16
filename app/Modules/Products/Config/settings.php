8a3c7394dfb62fd537029028242f3534ee24645a
Valera Boldarev <vboldarev@weltkind.com>
2023-08-16 14:38:52 +0600

    update products admin menu

--- app/Modules/Products/Config/settings.php
+++ app/Modules/Products/Config/settings.php
@@ -1,18 +1,24 @@
 <?php
 
 return [
+    'group' => [
+		'title' 	=> trans('Products::adminpanel.title'),
+		'name'		=> 'products_group',
+		'icon' 		=> 'fa fa-archive',
+		'priority' 	=> 4,
+	],
     'menu_items' => [
         [
-            'icon'      => 'fa fa-tasks',
+            'icon'      => 'fa fa-archive',
             'route'     => 'admin.products.index',
-            'group'     => 'main_group',
+            'group'     => 'products_group',
             'title'     => trans('Products::adminpanel.title'),
             'priority'  => 105,
         ],
         [
-            'icon'      => 'fa fa-tasks',
+            'icon'      => 'fa fa-list-ul',
             'route'     => 'admin.products_categories.index',
-            'group'     => 'main_group',
+            'group'     => 'products_group',
             'title'     => trans('Products::adminpanel.products_categories_title'),
             'priority'  => 106,
         ],
