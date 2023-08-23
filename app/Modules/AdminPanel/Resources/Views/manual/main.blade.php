5b8518de3458b103aaa065c60cd873bd505c4db2
Valera Boldarev <vboldarev@weltkind.com>
2023-08-23 12:36:18 +0600

    add sitemap manual

--- app/Modules/AdminPanel/Resources/Views/manual/main.blade.php
+++ app/Modules/AdminPanel/Resources/Views/manual/main.blade.php
@@ -19,5 +19,6 @@
         <li><a href="{{ route(config('cms.admin_prefix') . 'manual.show', ['type' => 'developers','name' => 'commands']) }}">Комманды</a></li>
         <li><a href="{{ route(config('cms.admin_prefix') . 'manual.show', ['type' => 'developers','name' => 'add_images']) }}">Добавление загрузки изображений и файлов в модуль</a></li>
         <li><a href="{{ route(config('cms.admin_prefix') . 'manual.show', ['type' => 'developers','name' => 'positions']) }}">Добавление позиционирования записей в модуль</a></li>
+        <li><a href="{{ route(config('cms.admin_prefix') . 'manual.show', ['type' => 'developers','name' => 'sitemap']) }}">Добавление индексирования сущностей модуля в sitemap</a></li>
     </ul>
 @endsection
