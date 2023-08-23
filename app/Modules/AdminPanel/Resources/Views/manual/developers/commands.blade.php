a560c250a00e76b2018ef20a56541e5a1b72a8e0
Valera Boldarev <vboldarev@weltkind.com>
2023-08-23 13:50:45 +0600

    update commands manusl

--- app/Modules/AdminPanel/Resources/Views/manual/developers/commands.blade.php
+++ app/Modules/AdminPanel/Resources/Views/manual/developers/commands.blade.php
@@ -16,6 +16,44 @@
 php artisan module:create {module_name}
 </code>
         </pre>
+
+        <p>Создаст структуру модуля:</p>
+        <pre>
+            <code>
+Module_name
+----Config
+--------settings.php
+----Database 
+--------Migrations
+------------Date('Y_m_d_hms') . '_create_' . $model_name . '_table.php' 
+--------Seeds
+------------ucfirst($model_name) . 'TableSeeder.php' 
+----Http 
+--------Controllers 
+------------Admin
+----------------IndexController.php
+------------IndexController.php 
+--------ViewComposers
+------------MainComposer.php 
+----Models
+--------ucfirst($model_name . '.php') 
+----Resources 
+--------Lang  
+------------ru 
+----------------adminpanel.php 
+----------------index.php 
+------------en 
+----------------index.php
+--------Views 
+------------admin
+------------  index.blade.php 
+----------------form.blade.php
+------------index.blade.php
+------------main.blade.php
+----Routes
+--------web.php 
+            </code>
+        </pre>
         <h3 id="clear_all">Очистить все(кэш, роуты, сессии ...)</h3>
         <pre>
 <code>
