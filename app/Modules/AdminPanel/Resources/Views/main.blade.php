560630d15258db26bdca94bff4b2afa3033b16a8
Valera Boldarev <vboldarev@weltkind.com>
2023-09-04 17:11:04 +0600

    search module part 3

--- app/Modules/AdminPanel/Resources/Views/main.blade.php
+++ app/Modules/AdminPanel/Resources/Views/main.blade.php
@@ -2,6 +2,5 @@
 
 @section('content')
 	Welcome to admin panel
-    @include('Search::admin.search')
     <a href="{{ route(config('cms.admin_prefix') . 'manual') }}" class="manual__link">Документация по ипользованию Админ панели</a>
 @endsection
